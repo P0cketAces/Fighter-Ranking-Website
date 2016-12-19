10e159fa6ed01fe3afafb2db7ce0704ba9bd2315
Joey Nelson <jnelson0112@gmail.com>
2016-12-19 16:47:43 +0000

    added code comments

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,5 @@
 class Contact < ActiveRecord::Base
+    # Contact form validations
     validates :name, presence: true
     validates :email, presence: true
     validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
